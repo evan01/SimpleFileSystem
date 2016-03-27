6dab2723b7df2fd2cb5ee8acd09827609c750c15
Evan Knox <eknoxmobile@gmail.com>
2016-03-27 12:49:05 -0400

    Adding the Api files

---
+++ SimpleFileSystem/sfs_api.c
@@ -0,0 +1,9 @@
+//
+//  sfs_api.c
+//  SimpleFileSystem
+//
+//  Created by Evan Knox on 2016-03-27.
+//  Copyright © 2016 NaliApplications. All rights reserved.
+//
+
+#include "sfs_api.h"
---
+++ SimpleFileSystem/sfs_api.h
@@ -0,0 +1,14 @@
+//
+//  sfs_api.h
+//  SimpleFileSystem
+//
+//  Created by Evan Knox on 2016-03-27.
+//  Copyright © 2016 NaliApplications. All rights reserved.
+//
+
+#ifndef sfs_api_h
+#define sfs_api_h
+
+#include <stdio.h>
+
+#endif /* sfs_api_h */
