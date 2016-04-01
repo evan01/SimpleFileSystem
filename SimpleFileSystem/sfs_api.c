71b47edd528ab059a29523ff64266a39803e21c8
Evan Knox <eknoxmobile@gmail.com>
2016-03-31 22:37:28 -0400

    Progress...

--- SimpleFileSystem/sfs_api.c
+++ SimpleFileSystem/sfs_api.c
@@ -12,17 +12,17 @@
 /* 
  Constants, only used when creating a brand new file system, to be replaced
  with possible */
+//TODO change these constants
 static char *diskName = "Evan_Knox_sfs";
-static const int blockSize = 1024;//TODO change these constants
+static const int blockSize = 1024;
 static const int totalBlocks = 4;
 
-//Pointer to free blocks
-int *freeBlocks;
-
-
 /* Super block pointers */
+char *superBlock;
+int *freeBlocks;//Array of all the free blocks
 
 
+//TODO create an array of inodes called rootDirectory, this will hold all the file/directory information
 //This function will create an in memory block filled with '-'
 void* createBlock(){
     char *ptr = malloc(blockSize);
@@ -34,19 +34,21 @@ void* createBlock(){
 }
 
 //This function will handle the writing to the disk and updating of data structures
-void write(int blk, int nblocks, void *buffer){
-
-    if(freeBlocks[blk]== 0){
+void writeToDisk(int blk, int nblocks, void *buffer){
+    //We can always overwrite the superBlock and the freeBlocks disk blocks
+    if(freeBlocks[blk]== 0 || blk == 0 || blk == totalBlocks-1){
         //Then yes there are free blocks
+        freeBlocks[blk] = '1';
         write_blocks(blk, nblocks, buffer);
     }else{
         //There are no more free blocks
-        printf("Sorry, no more free blocks\n");
+        printf("Sorry, the block requested B[%d/%d] isn't free\n",blk,totalBlocks);
     }
 
 }
 
 //This function will copy X number of bytes from the first pointer to the next
+//Pretty redundant actually given it just calls memcpy
 void copyBytes(int numBytes, void *src, void *dest){
         memcpy(dest,src,numBytes);
 }
@@ -56,31 +58,40 @@ void createBitMap(int numBlocks){
     //(Memory) Create a virtual array representing the free blocks, it'll have it's own block
     freeBlocks = createBlock();
     for (int i = 0; i < numBlocks; ++i) {
-        freeBlocks[i] = 0;
+        freeBlocks[i] = '0';
     }
-
-    //Then write this to disk
-    write(totalBlocks-1,1,freeBlocks);
 }
 
 //This function creates a super block in the first block
 void createSuperBlock(){
     //First create a properly formatted block in memory...
-    char *buffer = createBlock();
+    superBlock = createBlock();
 
     char magicVal = '7';
-    copyBytes(4,&magicVal,buffer);
-    buffer[13] = 'L';
+    copyBytes(4,&magicVal,superBlock);
+    superBlock[13] = 'L';
 
-    buffer[9] = '6';
-    buffer[10] = '9';
+    superBlock[9] = '6';
+    superBlock[10] = '9';
 
-    write(0,1,buffer);
 }
 
 //This function reads a pre-existing super block
 void readSuperBlock(){
+    //TODO implement this method
     
+    //First allocate the bit map
+}
+
+//This function will write the pre-existing data structures to disk
+void writeMemToDisk(){
+    
+    //Update the super block
+    writeToDisk(0,1,superBlock);
+    //Update the i-node Table
+    
+    //Write the bit-map to the disk last
+    writeToDisk(totalBlocks-1,1,freeBlocks);
 }
 
 
@@ -95,14 +106,17 @@ void mksfs(int fresh){
 	if (fresh>=1){
 		/* Then we initialize a new disk */
 		init_fresh_disk(diskName,blockSize,totalBlocks); //Creates the fresh disk of propper size
+        createBitMap(totalBlocks);//Creates the bit map block
         createSuperBlock();//Creates the super block, sends it to disk
-        createBitMap(totalBlocks);//Creates the bit map block, sends it to disk
+        
 	}else{
 		//Open an old disk, will use the same disk Name as before...
         init_disk(diskName, blockSize, totalBlocks);
         readSuperBlock();
+        
 	}
     //This means that the file pointer fp is now initialized, this represents our disk
+    writeMemToDisk();
 }
 
 
